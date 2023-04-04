0f1312e6dd543d8cbfbed40067149b93b6057a75
JosueRodriguez38 <josue.rodriguez38@upr.edu>
2023-04-04 16:45:37 -0300

    added database in main activity, names now recorded in login

--- MedCheck/app/src/main/java/com/example/medcheck/MainActivity.java
+++ MedCheck/app/src/main/java/com/example/medcheck/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private Button logout;
+    private EditText edit;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logout = findViewById(R.id.logout);
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_name = edit.getText().toString();
+                if (txt_name.isEmpty()){
+                    Toast.makeText(MainActivity.this, "No name entered", Toast.LENGTH_SHORT).show();
+                } else{
+                    FirebaseDatabase.getInstance().getReference().child("ProgrammingKnowledge").push().child("name").setValue(txt_name);
+                }
+            }
+        });
+
     }
 }
